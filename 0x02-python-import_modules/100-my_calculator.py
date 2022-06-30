62d5b9e05399340319e6a41a57a4c7799a382a62
Freifrei <freweyni19@gmail.com>
2022-06-30 06:00:16 -0700

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == '__main__':
     from calculator_1 import add, sub, div, mul
-
     import sys
-
     opert = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(sys.argv) -1 != 3:
@@ -17,7 +14,6 @@ if __name__ == '__main__':
     elif sys.argv[2] not in list(opert.keys()):
         print("unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
-
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
