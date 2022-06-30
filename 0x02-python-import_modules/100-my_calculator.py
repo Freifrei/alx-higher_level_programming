fed23ba8942ac542739912225edb95f35315af55
Freifrei <freweyni19@gmail.com>
2022-06-30 05:30:33 -0700

    import all function from module

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     opert = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(sys.argv) -1 != 3:
-        print("usage: ./100-my_calculator.py <a> <operator> <b>")
+         print("usage: ./100-my_calculator.py <a> <operator> <b>")
          sys.exit(1)
          # create an array of various operators
 
