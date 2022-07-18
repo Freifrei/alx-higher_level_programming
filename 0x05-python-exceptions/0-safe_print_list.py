2f655151b516f7e6d10988640f1e68984a33813b
Freifrei <Freweyni19@gmail.com>
2022-07-18 13:38:46 -0700

    update pint list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,7 +12,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             ret += 1
-        except IndexError:
+        except Exception as e::
             break
-    print("")
+    print()
     return (ret)
