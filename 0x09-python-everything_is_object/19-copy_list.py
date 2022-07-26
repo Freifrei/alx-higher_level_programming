704b191a98b46461bbce946da2deddf5c3686fe8
Freifrei <Freweyni19@gmail.com>
2022-07-27 01:18:37 +0300

    corrrection

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 def copy_list(l):
     return l.copy()
-=======
-<<<<<<< HEAD
-def copy_list(1):
-    return 1.copy()
-=======
-def copy(l):
-    return l.copy()
->>>>>>> dbcf227cd833b20f7321c31af77f30b30603ad80
->>>>>>> cb302022f2d48f4835e2b9ed90ae8d5b18d6bcc2
